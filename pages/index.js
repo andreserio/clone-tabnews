ad8a954ee49b2541ebfbc8428b38a1dc67a55bb5
andreserio <andre.rserio@gmail.com>
2023-10-08 13:54:01 +0000

    Adiciona os arquivos de manifesto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,31 @@
-function Home(){
-    return <h1>Teste 2</h1>
+import React from 'react';
+
+function Home() {
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.text}>Voe alto, seja leve 🎈</h1>
+    </div>
+  );
 }
+
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100vw',
+    height: '100vh',
+    backgroundColor: 'black',
+    overflow: 'hidden',
+    margin: 0,
+    padding: 0,
+  },
+  text: {
+    fontFamily: 'Poppins, sans-serif',
+    fontSize: '36px',
+    color: 'white',
+    textAlign: 'center',
+  },
+};
 
 export default Home;
